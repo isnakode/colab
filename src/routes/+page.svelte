8d3e68a572891c9b66988d3790ab28a054d0a763
isnakode <isnainisasaki@gmail.com>
2025-05-29 14:34:11 +0800

    tes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
    import { onMount } from "svelte";
    import { WebsocketProvider } from "y-websocket";
    import * as Y from "yjs";
@@ -16,14 +16,34 @@
    });
    onMount(() => {
       value = yText.toString();
+
+      yText.observe((event) => {
+         const newValue = yText.toString();
+         if (newValue !== value) {
+            value = newValue;
+         }
+      });
    });
+   function handleInput(e: Event) {
+      const newVal = (e.currentTarget as HTMLInputElement).value;
+
+      // Find the first diff index
+      let i = 0;
+      while (i < value.length && value[i] === newVal[i]) i++;
+
+      // Delete old chars
+      if (value.length > i) {
+         yText.delete(i, value.length - i);
+      }
+
+      // Insert new chars
+      if (newVal.length > i) {
+         yText.insert(i, newVal.slice(i));
+      }
+
+      // Update local value to reflect change immediately
+      value = newVal;
+   }
 </script>
 
-<input
-   type="text"
-   oninput={(e) => {
-      yText.delete(0, yText.length);
-      yText.insert(0, e.currentTarget.value);
-   }}
-   {value}
-/>
+<input type="text" oninput={handleInput} {value} />
